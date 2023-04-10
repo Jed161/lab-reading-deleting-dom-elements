1faca6072b391b66c17526833e5af92177a9d4a5
James Edmond <jamesedmond@pursuit.org>
2023-04-10 16:40:06 -0400

    committing main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector("li").remove()
+document.querySelector("span").remove()
+document.querySelector(".posts article").remove()
+
+let allTitles = document.querySelectorAll("h3")
+allTitles.forEach((title) => title.remove())
